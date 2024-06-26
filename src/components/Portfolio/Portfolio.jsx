761fbe0ef6055bc35c150dda097cb836505c1e65
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-06-26 18:07:07 +0530

    urlChanges

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -2,72 +2,101 @@ import React from "react";
 
 import i1 from "../../assets/portfolio/bint1.png";
 import i2 from "../../assets/portfolio/fab1.png";
-import i3 from "../../assets/portfolio/i4.jpg";
+import i3 from "../../assets/portfolio/sriAmbikaPlast.jpg";
 import i4 from "../../assets/portfolio/i6.jpg";
-import i5 from "../../assets/portfolio/p3.png";
+import i5 from "../../assets/portfolio/solar-fintech.jpg";
 import i6 from "../../assets/portfolio/playpaws.jpg";
 import i7 from "../../assets/portfolio/silveLine2.jpg";
 import i8 from "../../assets/portfolio/innomatricsSite.jpg";
 import i9 from "../../assets/portfolio/billingSoft.jpg";
+import i10 from "../../assets/portfolio/casino-game.jpg";
 
 import doodle from "../../assets/portfolioBg.jpg";
 import { BackgroundBeams } from "../ui/background-beams";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const Portfolio = () => {
   const data = [
-    {
-      color: "orange",
-      image: i1,
-      title: "Bint Al Hind",
-      desc: "An incense stick e commerse website displayinng their products adn allowing client to reach out to them for contact.",
-    },
+    // {
+    //   color: "orange",
+    //   image: i1,
+    //   url: "" ,
+    //   title: "Bint Al Hind",
+    //   desc: "An incense stick e commerse website displayinng their products adn allowing client to reach out to them for contact.",
+    // },
     {
       image: i2,
       color: "sky",
+      url: "https://www.kkindustrie.com/" ,
+      gitUrl:"",
       title: "KK Industries",
       desc: "An online website for a fabricaionn industry with description and access to their services.",
     },
     {
       image: i3,
       color: "violet",
-      title: "Infinty Travels",
-      desc: "A travels and transportation business with features to booka ride and more.",
+      url: "https://sri-ambika-plast.vercel.app/" ,
+      gitUrl:"",
+      title: "Shri Ambika Plast",
+      desc: "A Pipe manufacturer business with their business products and quality description.",
     },
     {
       image: i4,
       color: "rose",
+      url: "" ,
+      gitUrl:"",
       title: "Poke Store",
       desc: "A client side web design for an online Poke dtore owned by an European client.",
     },
     {
       image: i5,
+      url: "https://solar-fintech.vercel.app/" ,
       color: "amber",
-      title: "Alwyn Packers & Movers    ",
-      desc: "A Packers and movers website with features to contact; view videos and avail services.",
+      gitUrl:"",
+      title: "Solar Fintech",
+      desc: "A Solar Fintech based startup website designed as per client's requirements",
     },
     {
       image: i6,
       color: "indigo",
+      url: "https://playpawws.com/" ,
+      gitUrl:"",
       title: "Play Pawws",
       desc: "Online business website for kid's play zone arena with subscription features.",
     },
     {
       image: i7,
       color: "red",
+      url: "https://silverlinetoursandtravels.com/" ,
+      gitUrl:"",
       title: "Silverline Tours & Travels",
       desc: "A travels and tours company whose business website was built using cutting edge tech and making it user centric.",
     },
     {
       image: i8,
       color: "yellow",
+      url: "innomatricstech.com" ,
+      gitUrl:"",
       title: "Innomatrics Technologies",
       desc: "A client side business wensite for a tech consultant company.",
     },
     {
       image: i9,
-      color: "green",
+      color: "indigo",
+      url: "https://inno-bill-client.vercel.app/" ,
+      gitUrl:"",
       title: "Innomatrics Billing Software",
       desc: "A Full stack billing software/app to track and generate invoice for clients.",
+    },
+    {
+      image: i10,
+      color: "red",
+      url: "https://casino-game-ochre.vercel.app/" ,
+      gitUrl:"",
+      title: "Casino ree play game",
+      desc: "A Casino based point system game created using React.",
     },
   ];
   return (
@@ -113,24 +142,31 @@ const Portfolio = () => {
           <div className="grid grid-cols-1 md:grid-cols-3  gap-8 ">
             {/* Card 1 */}
             {data.map((item, index) => (
+              <Link to={item.url} target="_blank" key={index}>
               <div
-                key={index}
+                
                 className={`bg-white rounded-lg shadow-lg shadow-slate-500 overflow-hidden h-96 
         hover:bg-${item.color}-400 hover:transform hover:scale-105
               hover:shadow-lg transition duration-300 ease-in-out`}
               >
                 <img
-                  src={item.image}
+                  src={item.image} 
                   alt="Placeholder"
                   className="w-full rounded h-2/3"
                 />
-                <div className="p-6">
+                <div className="p-6 flex flex-row">
+                  <div>
                   <h3 className="text-xl text-black font-semibold mb-2">
                     {item.title}{" "}
                   </h3>
                   <p className="text-gray-700 text-sm">.{item.desc}</p>
+                  </div>
+                  <Link to={gitUrl} target="_blank">
+                  <FontAwesomeIcon icon={faGithub}/>
+                  </Link>
                 </div>
               </div>
+              </Link>
             ))}
           </div>
         </div>
